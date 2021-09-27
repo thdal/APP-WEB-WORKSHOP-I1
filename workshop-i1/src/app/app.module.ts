2f8b92275b6cc7b02b04ba808612585bab316cba
Skeedy <pierre_tisserand@orange.fr>
2021-09-27 15:50:05 +0200

    2 components youpi !

--- workshop-i1/src/app/app.module.ts
+++ workshop-i1/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlockChatBotComponent } from './components/block-chat-bot/block-chat-bot.component';
+import { BlockHeaderComponent } from './components/block-header/block-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockChatBotComponent,
+    BlockHeaderComponent
   ],
   imports: [
     BrowserModule
