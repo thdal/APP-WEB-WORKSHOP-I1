e88bf7fa26f012756da39bbe558080611d1c2e80
TifSP <tpoinot17@gmail.com>
2021-09-28 14:35:22 +0200

    ajout des differentes parties du corps du site

--- workshop-i1/src/app/app.module.ts
+++ workshop-i1/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BlockChatBotComponent } from './components/block-chat-bot/block-chat-bot.component';
 import { BlockHeaderComponent } from './components/block-header/block-header.component';
-import { BlockFooterComponent } from './coponents/block-footer/block-footer.component';
+import { BlockFooterComponent } from './components/block-footer/block-footer.component';
+import { BlockBodyComponent } from './components/block-body/block-body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockChatBotComponent,
     BlockHeaderComponent,
-    BlockFooterComponent
+    BlockFooterComponent,
+    BlockBodyComponent
   ],
   imports: [
     BrowserModule
---
+++ workshop-i1/src/app/components/block-body/block-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-body',
+  templateUrl: './block-body.component.html',
+  styleUrls: ['./block-body.component.css']
+})
+export class BlockBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
