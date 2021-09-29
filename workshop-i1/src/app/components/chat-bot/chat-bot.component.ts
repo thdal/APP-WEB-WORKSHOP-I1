a12f8be7fc8dbcb5727b07be5ec400cd0bc1c3da
Julian Lacher <jlacherfougere@gmail.com>
2021-09-29 17:34:38 +0200

    chatbot dynamic mood

--- workshop-i1/src/app/components/chat-bot/chat-bot.component.ts
+++ workshop-i1/src/app/components/chat-bot/chat-bot.component.ts
@@ -15,8 +15,11 @@ export class ChatBotComponent implements OnInit {
   public arms: any;
   public mark: any;
   public beam: any;
+  public mood: any;
+  public observer: any;
+  public test:any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.green = document.querySelector('.vert');
@@ -24,64 +27,72 @@ export class ChatBotComponent implements OnInit {
     this.red = document.querySelector('.rouge');
     this.hello = document.querySelector('.bonjour');
     this.writing = document.querySelector('.ecrire');
-    this.eyes = document.querySelectorAll('.chatbot-eye-green');
-    this.arms = document.querySelectorAll('.chatbot-arm-hello');
-    this.mark = document.querySelector('.chatbot-mark-green');
-    this.beam = document.querySelector('#chatbot-beam-pulsar');
+    this.mood = document.querySelector('#peekobot-mood');
+    this.observer = new MutationObserver(function (event) {
+      this.eyes = document.querySelectorAll('.chatbot-eye');
+      this.arms = document.querySelectorAll('.chatbot-arm');
+      this.mark = document.querySelector('.chatbot-mark');
+      this.beam = document.querySelector('#chatbot-beam-pulsar');
+      if (event[0].target.className) {
+        switch (event[0].target.className) {
+          case 'green':
+            this.eyes.forEach(eye => {
+              eye.className = 'chatbot-eye chatbot-eye-green centered';
+            });
+            this.arms.forEach(arm => {
+              arm.className = 'chatbot-arm chatbot-arm-green';
+            });
+            this.mark.className = 'chatbot-mark chatbot-mark-green';
+            this.beam.className = 'chatbot-beam-green';
+            break;
+
+            case 'orange':
+              this.eyes.forEach(eye => {
+              eye.className = 'chatbot-eye chatbot-eye-orange centered';
+            });
+            this.arms.forEach(arm => {
+              arm.className = 'chatbot-arm chatbot-arm-orange';
+            });
+            this.mark.className = 'chatbot-mark chatbot-mark-orange';
+            this.beam.className = 'chatbot-beam-orange';
+            break;
+        
+            case 'red':
+              this.eyes.forEach(eye => {
+              eye.className = 'chatbot-eye chatbot-eye-red centered';
+            });
+            this.arms.forEach(arm => {
+              arm.className = 'chatbot-arm chatbot-arm-red';
+            });
+            this.mark.className = 'chatbot-mark chatbot-mark-red';
+            this.beam.className = 'chatbot-beam-red';
+            break;
+        
+            case 'hello':
+              this.eyes.forEach(eye => {
+              eye.className = 'chatbot-eye chatbot-eye-green centered';
+            });
+            this.arms.forEach(arm => {
+              arm.className = 'chatbot-arm chatbot-arm-hello';
+            });
+            this.mark.className = 'chatbot-mark chatbot-mark-green';
+            this.beam.className = 'chatbot-beam-green';
+            break;
+        
+            case 'writing':
+              this.eyes.forEach(eye => {
+              eye.className = 'chatbot-eye chatbot-eye-writing centered';
+            });
+            this.mark.className = 'chatbot-mark chatbot-mark-writing';
+            break;
+          }
+        }
+    });
+    this.observer.observe(this.mood, {
+      attributes: true, 
+      attributeFilter: ['class'],
+      childList: false, 
+      characterData: false
+    })
   }
-
-  greenEye() {
-    this.eyes.forEach(eye => {
-      eye.className = 'chatbot-eye-green centered';
-    });
-    this.arms.forEach(arm => {
-      arm.className = 'chatbot-arm-green';
-    });
-    this.mark.className = 'chatbot-mark-green';
-    this.beam.className = 'green';
-  }
-
-  orangeEye() {
-    this.eyes.forEach(eye => {
-      eye.className = 'chatbot-eye-orange centered';
-    });
-    this.arms.forEach(arm => {
-      arm.className = 'chatbot-arm-orange';
-    });
-    this.mark.className = 'chatbot-mark-orange';
-    this.beam.className = 'orange';
-  }
-
-  redEye() {
-    this.eyes.forEach(eye => {
-      eye.className = 'chatbot-eye-red centered';
-    });
-    this.arms.forEach(arm => {
-      arm.className = 'chatbot-arm-red';
-    });
-    this.mark.className = 'chatbot-mark-red';
-    this.beam.className = 'red';
-  }
-
-  sayHello() {
-    this.eyes.forEach(eye => {
-      eye.className = 'chatbot-eye-green centered';
-    });
-    this.arms.forEach(arm => {
-      arm.className = 'chatbot-arm-hello';
-    });
-    this.mark.className = 'chatbot-mark-green';
-    this.beam.className = 'green';
-  }
-
-  write() {
-    this.eyes.forEach(eye => {
-      eye.className = 'chatbot-eye-writing centered';
-    });
-    this.mark.className = 'chatbot-mark-writing';
-  }
-
-
-
-
 }
